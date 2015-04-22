62c61d17f71495cdd0ddf39cfd97cb22f7d7bd30
MadCake <wdiabloster@gmail.com>
2015-04-22 11:11:05 +0600

    Добавленно сохранение позиции. Код не проверен.

--- app/src/main/java/com/dzencake/slidingpane/VerticalLayout.java
+++ app/src/main/java/com/dzencake/slidingpane/VerticalLayout.java
@@ -1,20 +1,36 @@
 package com.dzencake.slidingpane;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
 
+import static android.support.v7.widget.RecyclerView.NO_POSITION;
+
 public class VerticalLayout extends RecyclerView.LayoutManager {
 
 	private static final String TAG = VerticalLayout.class.getSimpleName();
 
 	private OrientationHelper mOrientationHelper;
+	private SavedState mPendingSavedState;
 
 	public VerticalLayout() {
 		// Враппер над LayoutParams с хелпер методами работы с конкретным View
 		mOrientationHelper = OrientationHelper.createVerticalHelper(this);
+	}
+
+	@Override
+	public void onRestoreInstanceState(Parcelable state) {
+		if (state instanceof SavedState) {
+			mPendingSavedState = (SavedState) state;
+			if (mPendingSavedState.offset > getHeight()) {
+				mPendingSavedState.offset = getHeight(); // TODO: У нас может быть и больше
+			}
+			requestLayout();
+		}
 	}
 
 	@Override
@@ -25,13 +41,22 @@ public class VerticalLayout extends RecyclerView.LayoutManager {
 
 	@Override
 	public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
-		int oldFirstPos = getChildCount() == 0 ? 0 : getPosition(getChildAt(0));
-		// Считаем оффсет
-		int offset = getChildCount() == 0 ? 0 : mOrientationHelper.getDecoratedStart(getChildAt(0));
+		int oldFirstPos;
+		int offset;
+		if (mPendingSavedState == null || !mPendingSavedState.hasValidPosition()) {
+			oldFirstPos = getChildCount() == 0 ? 0 : getPosition(getChildAt(0));
+			// Считаем оффсет
+			offset = getChildCount() == 0 ? 0 : mOrientationHelper.getDecoratedStart(getChildAt(0));
+		} else {
+			oldFirstPos = mPendingSavedState.position;
+			offset = mPendingSavedState.offset;
+		}
 		detachAndScrapAttachedViews(recycler);
 		// Заполняем
 		int consumed = fill(recycler, 1, offset, oldFirstPos, 0);
 		offsetChildrenVertical(-consumed);
+		// Больше не требуется
+		mPendingSavedState = null;
 	}
 
 	@Override
@@ -63,7 +88,7 @@ public class VerticalLayout extends RecyclerView.LayoutManager {
 	}
 
 	private int fill(RecyclerView.Recycler recycler, int direction, int offset, int oldPos, int dy) {
-		int consumed = 0;
+		int consumed;
 		// Заводим кэш
 		SparseArray<View> cache = new SparseArray<>();
 		int childCount = getChildCount();
@@ -152,5 +177,76 @@ public class VerticalLayout extends RecyclerView.LayoutManager {
 			recycler.recycleView(cache.valueAt(i));
 		}
 		return consumed;
+	}
+
+	@Override
+	public Parcelable onSaveInstanceState() {
+		if (mPendingSavedState != null) {
+			return new SavedState(mPendingSavedState);
+		}
+		SavedState state = new SavedState();
+		if (getChildCount() > 0) {
+			state.position = getPosition(getChildAt(0));
+			state.offset = mOrientationHelper.getDecoratedStart(getChildAt(0));
+//			TODO:
+//			state.offset = mOrientationHelper.getDecoratedStart(refChild) -
+//					mOrientationHelper.getStartAfterPadding();
+		} else {
+			state.invalidatePosition();
+		}
+		return state;
+	}
+
+	static class SavedState implements Parcelable {
+
+		int position;
+
+		int offset;
+
+		public SavedState() {
+
+		}
+
+		SavedState(Parcel in) {
+			position = in.readInt();
+			offset = in.readInt();
+		}
+
+		public SavedState(SavedState other) {
+			position = other.position;
+			offset = other.offset;
+		}
+
+		boolean hasValidPosition() {
+			return position >= 0;
+		}
+
+		void invalidatePosition() {
+			position = NO_POSITION;
+		}
+
+		@Override
+		public int describeContents() {
+			return 0;
+		}
+
+		@Override
+		public void writeToParcel(Parcel dest, int flags) {
+			dest.writeInt(position);
+			dest.writeInt(offset);
+		}
+
+		public static final Parcelable.Creator<SavedState> CREATOR
+				= new Parcelable.Creator<SavedState>() {
+			@Override
+			public SavedState createFromParcel(Parcel in) {
+				return new SavedState(in);
+			}
+
+			@Override
+			public SavedState[] newArray(int size) {
+				return new SavedState[size];
+			}
+		};
 	}
 }
