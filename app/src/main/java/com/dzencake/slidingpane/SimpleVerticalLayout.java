fd6ed3df1476b6c6c608e7713b6e5a2abccc88bf
MadCake <wdiabloster@gmail.com>
2015-04-28 14:29:41 +0600

    Fixed gaps

--- app/src/main/java/com/dzencake/slidingpane/SimpleVerticalLayout.java
+++ app/src/main/java/com/dzencake/slidingpane/SimpleVerticalLayout.java
@@ -31,21 +31,21 @@ public class SimpleVerticalLayout extends RecyclerView.LayoutManager {
 	public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
 		int position = 0;
 		int offset = 0;
-		if (getChildCount() > 0){
+		if (getChildCount() > 0) {
 			position = getPosition(getChildAt(0));
-			offset = getChildAt(0).getTop();
+			offset = getViewTop(getChildAt(0));
 		}
 
 		detachAndScrapAttachedViews(recycler);
 
-		if (getItemCount() == 0){
+		if (getItemCount() == 0) {
 			return;
 		}
 
-		fillEndGap(recycler, position, offset);
+		fillEndGap(recycler, position, offset, 0);
 
 		if (getChildCount() > 0) {
-			int viewsEnd = getChildAt(getChildCount() - 1).getBottom();
+			int viewsEnd = getViewBottom(getChildAt(getChildCount() - 1));
 			if (viewsEnd < getHeight()) {
 				offsetChildrenVertical(getHeight() - viewsEnd);
 			}
@@ -53,15 +53,15 @@ public class SimpleVerticalLayout extends RecyclerView.LayoutManager {
 
 		position = 0;
 		offset = 0;
-		if (getChildCount() > 0){
-			position = getPosition(getChildAt(0));
-			offset = getChildAt(0).getTop();
+		if (getChildCount() > 0) {
+			position = getPosition(getChildAt(0)) - 1;
+			offset = getViewTop(getChildAt(0));
 		}
 
-		fillStartGap(recycler, position, offset);
+		fillStartGap(recycler, position, offset, 0);
 
-		int viewsStart = getChildAt(0).getTop();
-		if (viewsStart > 0){
+		int viewsStart = getViewTop(getChildAt(0));
+		if (viewsStart > 0) {
 			offsetChildrenVertical(-viewsStart);
 		}
 	}
@@ -246,11 +246,13 @@ public class SimpleVerticalLayout extends RecyclerView.LayoutManager {
 	/**
 	 * Метод заполняет пустое место вьюхами, начиная с position со сдвигом topOffset. При добавлении View, offset и position
 	 * будут увеличиваться, пока offset не станет больше getHeight()
-	 * @param position позиция вьюшки в адаптере, с которой нужно начать заполнение
-	 * @param topOffset изначальный сдвиг (topOffset будет равен getTop() у первой View, которая добавится через этот метод)
+	 *
+	 * @param position      позиция вьюшки в адаптере, с которой нужно начать заполнение
+	 * @param topOffset     изначальный сдвиг (topOffset будет равен getTop() у первой View, которая добавится через этот метод)
+	 * @param additionalGap дополнительное место для заполнения вьюшек
 	 */
-	public void fillEndGap(RecyclerView.Recycler recycler, int position, int topOffset) {
-		while (position < getItemCount() && topOffset < getHeight()) {
+	private void fillEndGap(RecyclerView.Recycler recycler, int position, int topOffset, int additionalGap) {
+		while (position < getItemCount() && topOffset < getHeight() + additionalGap) {
 			View v = recycler.getViewForPosition(position);
 			measureChildWithMargins(v, 0, 0);
 
@@ -269,20 +271,22 @@ public class SimpleVerticalLayout extends RecyclerView.LayoutManager {
 	/**
 	 * Метод заполняет пустое место вьюхами, начиная с position со сдвигом (bottomOffset - viewHeight). При добавлении View, offset и position
 	 * будут уменьшаться, пока offset не станет меньше 0.
-	 * @param position позиция вьюшки в адаптере, с которой нужно начать заполнение
-	 * @param bottomOffset изначальный сдвиг (bottomOffset будет равен getBottom() у первой View, которая добавится через этот метод)
+	 *
+	 * @param position      позиция вьюшки в адаптере, с которой нужно начать заполнение
+	 * @param bottomOffset  изначальный сдвиг (bottomOffset будет равен getBottom() у первой View, которая добавится через этот метод)
+	 * @param additionalGap дополнительное место для заполнения вьюшек
 	 */
-	public void fillStartGap(RecyclerView.Recycler recycler, int position, int bottomOffset) {
-		if (position >= getItemCount()){
+	private void fillStartGap(RecyclerView.Recycler recycler, int position, int bottomOffset, int additionalGap) {
+		if (position >= getItemCount()) {
 			position = getItemCount() - 1;
 		}
-		while (position > -1 && bottomOffset > 0) {
+		while (position > -1 && bottomOffset > -additionalGap) {
 			View v = recycler.getViewForPosition(position);
 			measureChildWithMargins(v, 0, 0);
 
 			int viewHeight = mOrientationHelper.getDecoratedMeasurement(v);
 
-			addView(v);
+			addView(v, 0);
 			RecyclerView.LayoutParams lp = (RecyclerView.LayoutParams) v.getLayoutParams();
 			layoutDecorated(v, lp.leftMargin, bottomOffset - viewHeight + lp.topMargin,
 					getWidth() - lp.rightMargin, bottomOffset - lp.bottomMargin);
@@ -300,6 +304,14 @@ public class SimpleVerticalLayout extends RecyclerView.LayoutManager {
 			state.offset = mOrientationHelper.getDecoratedStart(getChildAt(0));
 		}
 		return state;
+	}
+
+	private int getViewTop(View view) {
+		return mOrientationHelper.getDecoratedStart(view);
+	}
+
+	private int getViewBottom(View view) {
+		return mOrientationHelper.getDecoratedEnd(view);
 	}
 
 	private static class LayoutState {
